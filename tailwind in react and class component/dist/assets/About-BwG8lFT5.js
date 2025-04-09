dac8666124cc491493b563defc7f07ed142714b6
Lovey Chauhan <143213952+loveychauhan@users.noreply.github.com>
2025-04-09 23:44:26 +0530

    Add files via upload

---
+++ tailwind in react and class component/dist/assets/About-BwG8lFT5.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DWLI9wh1.js";function r(){return e.jsx("div",{className:"text-center text-xl",children:"Hi there, my name is lovey"})}export{r as default};
---
+++ tailwind in react and class component/dist/assets/Contacts-DffraIhe.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DWLI9wh1.js";function n(){return t.jsx("div",{className:"text-center text-xl",children:"let's Connet on LinkedIn"})}export{n as default};
